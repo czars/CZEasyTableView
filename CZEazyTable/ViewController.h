9101fc6e827d570536024acea97703f2902a69e5
paul <czarsterchou@gmail.com>
2015-11-26 20:39:03 +0800

    Initial Commit

---
+++ CZEazyTable/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CZEazyTable
+//
+//  Created by PaulChou on 2015/11/26.
+//  Copyright © 2015年 CzarsStudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CZEazyTable/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CZEazyTable
+//
+//  Created by PaulChou on 2015/11/26.
+//  Copyright © 2015年 CzarsStudio. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
